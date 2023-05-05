19735016bab9737dd956cec3c7d6b42a72c7f321
lpanigrahi <89903479+lpanigrahi@users.noreply.github.com>
2023-05-06 00:28:06 +0530

    Main

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Python and Panda use for spread sheet comparison
+# Python and Panda use for spread sheet comparison
 #################################################
 import gradio as gr
 import pandas as pd
@@ -9,7 +9,7 @@ from collections import defaultdict
 cach_2 = pd.DataFrame()
 
 
-def dataset_display(file1,file2):
+def dataset_display(file1, file2):
     print('Processing Start')
     global cach_1
     global cach_2
@@ -19,56 +19,63 @@ def dataset_display(file1,file2):
 
     split_tup_1 = os.path.splitext(file1.name)
     split_tup_2 = os.path.splitext(file2.name)
-  
-    if (split_tup_1[1] and split_tup_2[1]) == '.xlsx' :
-        try:
-            df2=pd.read_excel(file2.name,dtype=str)
-            for i in range(len(df2.index)):
-                dict2[list(df2[i:i+1].ACCOUNT_NAME)[0]].append((list(df2[i:i+1]['ENTITLEMENT VALUE'])[0]))
 
-            df1=pd.read_excel(file1.name,dtype=str)
+    if (split_tup_1[1] and split_tup_2[1]) == '.xlsx':
+        try:
+            df2 = pd.read_excel(file2.name, dtype=str)
+            for i in range(len(df2.index)):
+                dict2[list(df2[i:i+1].ACCOUNT_NAME)[0]
+                      ].append((list(df2[i:i+1]['ENTITLEMENT VALUE'])[0]))
+
+            df1 = pd.read_excel(file1.name, dtype=str)
             for i in range(len(df1.index)):
-                dict1[list(df1[i:i+1].UNAME)[0]].append((list(df1[i:i+1].AGR_NAME)[0]))
+                dict1[list(df1[i:i+1].UNAME)[0]
+                      ].append((list(df1[i:i+1].AGR_NAME)[0]))
 
         except:
-            df2=pd.read_csv(file2.name,dtype=str)
+            df2 = pd.read_csv(file2.name, dtype=str)
             for i in range(len(df2.index)):
-                dict2[list(df2[i:i+1].ACCOUNT_NAME)[0]].append((list(df2[i:i+1]['ENTITLEMENT VALUE'])[0]))
+                dict2[list(df2[i:i+1].ACCOUNT_NAME)[0]
+                      ].append((list(df2[i:i+1]['ENTITLEMENT VALUE'])[0]))
 
-            df1=pd.read_csv(file1.name,dtype=str)
+            df1 = pd.read_csv(file1.name, dtype=str)
             for i in range(len(df1.index)):
-                dict1[list(df1[i:i+1].UNAME)[0]].append((list(df1[i:i+1].AGR_NAME)[0]))
-    
+                dict1[list(df1[i:i+1].UNAME)[0]
+                      ].append((list(df1[i:i+1].AGR_NAME)[0]))
+
     # Output file
     rows = []
     for item in list(dict1.keys()):
         row = []
         row.append(item)
         if dict2[item] == []:
-          row.append(['Missing-Value'])
-          row.append(dict1[item])
-          row.append(['No_AccessNow_Data'])
+            row.append(['Missing-Value'])
+            row.append(dict1[item])
+            row.append(['No_AccessNow_Data'])
         else:
-          row.append(item)
-          row.append(dict1[item])
-          if (list(set(dict1[item]).difference(dict2[item]))) == []:
-            row.append(['Matches'])
-          else:
-            row.append(list(set(dict1[item]).difference(dict2[item])))
+            row.append(item)
+            row.append(dict1[item])
+            if (list(set(dict1[item]).difference(dict2[item]))) == []:
+                row.append(['Matches'])
+            else:
+                row.append(list(set(dict1[item]).difference(dict2[item])))
 
         rows.append(row)
 
-    out2 = pd.DataFrame(rows,columns = ['UNAME','ACCOUNT_NAME','AGRNAME','MISSING ENTITLEMENT VALUE'])
+    out2 = pd.DataFrame(
+        rows, columns=['UNAME', 'ACCOUNT_NAME', 'AGRNAME', 'MISSING ENTITLEMENT VALUE'])
     print('Out2 processed')
     cach_2 = out2
     print('Processing Finished')
     output = out2[30:60]
     return output
 
+
 def export_csv():
 
     cach_2.to_csv("Processed_output.csv")
     return gr.File.update(value="Processed_output.csv", visible=True)
+
 
 with gr.Blocks() as demo:
     gr.Markdown("# xlCompare ")
@@ -92,8 +99,10 @@ with gr.Blocks() as demo:
     with gr.Tab("Upload the files"):
         gr.Markdown("##")
         with gr.Row():
-            inp1 = gr.File(label="Input File-1 / SAP_SP1_Data",file_types=['.xlsx','.csv','.txt'],interactive=True)
-            inp2 = gr.File(label="Input File-2 / AccessNow_Data",file_types=['.xlsx','.csv','.txt'],interactive=True)
+            inp1 = gr.File(label="Input File-1 / SAP_SP1_Data",
+                           file_types=['.xlsx', '.csv', '.txt'], interactive=True)
+            inp2 = gr.File(label="Input File-2 / AccessNow_Data",
+                           file_types=['.xlsx', '.csv', '.txt'], interactive=True)
 
         with gr.Row():
             gen_btn = gr.Button(" File Compare ")
@@ -102,20 +111,19 @@ with gr.Blocks() as demo:
             gen_btn_2 = gr.Button(" File Download ")
             csv2 = gr.File(interactive=False, visible=False)
 
-        with gr.Row():    
+        with gr.Row():
             gen_btn.click(
                 dataset_display,
-                inputs=[inp1,inp2],
+                inputs=[inp1, inp2],
                 outputs=[gr.Dataframe(
-                        label = "File Compare Data",
-                        headers=['UNAME','ACCOUNT_NAME','AGRNAME','MISSING ENTITLEMENT VALUE'],
-                        datatype=["str", "str","str", "str"],
-                        col_count=(4, "fixed"))]
-#                         max_rows = 30)]
-             )
+                    label="File Compare Data",
+                    headers=['UNAME', 'ACCOUNT_NAME',
+                             'AGRNAME', 'MISSING ENTITLEMENT VALUE'],
+                    datatype=["str", "str", "str", "str"],
+                    col_count=(4, "fixed"))]
+            )
 
         gen_btn_2.click(export_csv, outputs=[csv2])
 
 if __name__ == "__main__":
-    demo.launch(server_name='0.0.0.0',server_port=7000)
-
+    demo.launch(server_name='0.0.0.0', server_port=8000)
